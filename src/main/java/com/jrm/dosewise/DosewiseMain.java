4f3ae6e67d42f9ba02280246defc2589b32ef687
Jimena García <a19203731@alumnos.uady.mx>
2023-11-28 23:38:23 -0600

    Fix bug in login functionality

--- src/main/java/com/jrm/dosewise/DosewiseMain.java
+++ src/main/java/com/jrm/dosewise/DosewiseMain.java
@@ -1,15 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.jrm.dosewise;
 
 import java.util.Scanner;
 
+<<<<<<< Updated upstream
 /**
  *
  * @author jeusm
  */
+=======
+import com.jrm.dosewise.model.UserMedication;
+import com.jrm.dosewise.controller.UserMedicationController;
+
+
+
+>>>>>>> Stashed changes
 public class DosewiseMain {
 
     public static void main(String[] args) {    
