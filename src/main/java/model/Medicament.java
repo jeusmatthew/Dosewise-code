d07c8b98b940000a49c4382462abdb7c7d99b07a
jeusmatthew <jeusmateo@gmail.com>
2023-12-10 00:43:45 -0600

    Add id field to Medicament class

--- src/main/java/model/Medicament.java
+++ src/main/java/model/Medicament.java
@@ -1,30 +1,56 @@
 package model;
 
 public class Medicament {
+    private int id;
     private String name;
     private String description;
     private String compound;
+
+    public Medicament() {
+
+    }
+
+    public Medicament(int id, String name, String description, String compound) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.compound = compound;
+    }
 
     public Medicament(String name, String description, String compound) {
         this.name = name;
         this.description = description;
         this.compound = compound;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public String getCompound() {
         return compound;
     }
+
     public void setCompound(String compound) {
         this.compound = compound;
     }
