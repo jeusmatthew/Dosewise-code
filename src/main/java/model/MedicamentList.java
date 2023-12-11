7d5f6fd35f5b8ea2d720ac052a579f3612a74bd4
jeusmatthew <jeusmateo@gmail.com>
2023-12-10 18:04:27 -0600

    Refactor MedicamentList class to add new methods for managing medications

--- src/main/java/model/MedicamentList.java
+++ src/main/java/model/MedicamentList.java
@@ -3,7 +3,6 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class MedicamentList {
     private List<AlarmsRoutine> medications = new ArrayList<>();
 
@@ -18,17 +17,29 @@ public class MedicamentList {
 
     public void removeMedication(int index) {
         medications.remove(index);
-    }  
+    }
+
+    public void removeAllMedicaments() {
+        medications.clear();
+    }
 
     public void updateMedication(int index, AlarmsRoutine medication) {
         medications.set(index, medication);
     }
 
-    public List<AlarmsRoutine> getMedications() {
+    public AlarmsRoutine getMedicament(int index) {
+        return medications.get(index);
+    }
+
+    public void setMedicament(int index, AlarmsRoutine medication) {
+        medications.set(index, medication);
+    }
+
+    public List<AlarmsRoutine> getAllMedications() {
         return medications;
     }
 
-    public void setMedications(List<AlarmsRoutine> medications) {
+    public void setAllMedications(List<AlarmsRoutine> medications) {
         this.medications = medications;
     }
 
